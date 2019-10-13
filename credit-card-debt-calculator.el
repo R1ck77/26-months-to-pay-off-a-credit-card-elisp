b6cccedfc3b044973519fcc012f39836ed46dd74
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-10-13 16:09:12 +0200

    Minor refactoring

--- credit-card-debt-calculator.el
+++ credit-card-debt-calculator.el
@@ -11,6 +11,35 @@
                                 (/ apr 100.0)
                                 monthly-payment)))
 
-(defun credit-card-calculator-mode ())
+(defvar credit-card-calculator-mode nil
+  "Mode variable for \"Credit Card Calculator Mode\"")
+
+(defvar credit-card-mode-hook nil
+  "Hook functions for \"Credit Card Calculator Mode\"")
+
+(defconst ccdc--mode-name "Credit Card Calculator Mode")
+(defconst ccdc--buffer-name (concat "* " ccdc--mode-name " *"))
+
+(defun ccdc--get-new-buffer ()
+  (let ((old-buffer (get-buffer ccdc--buffer-name)))
+    (when old-buffer
+      (kill-buffer old-buffer))
+    (get-buffer-create ccdc--buffer-name)))
+
+(defun ccdc--switch-to-new-buffer ()
+  (let ((buffer (ccdc--get-new-buffer)))
+    (display-buffer buffer)
+    (set-buffer buffer)))
+
+(defun ccdc--mode ()
+  (kill-all-local-variables)  
+  (setq major-mode 'credit-card-calculator-mode)
+  (setq mode-name ccdc--mode-name)
+  (run-hooks 'credit-card-mode-hook))
+
+(defun credit-card-compute-months ()
+  (interactive)
+  (ccdc--switch-to-new-buffer)
+  (ccdc--mode))
 
 (provide 'credit-card-debt-calculator)
